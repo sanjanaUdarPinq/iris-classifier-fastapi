9199b9560c53841cfb537e763342fd62ac9eb640
Felix Simard <felixsimard@gmail.com>
2024-08-05 09:16:18 -0400

    Iris classifier using FastAPI and Docker

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,7 @@
-FROM python:3.7-slim-buster
-LABEL maintainer="mandy -- follow me on medium https://medium.com/@mandygu"
-
-RUN apt-get update && apt-get install -y python3-dev build-essential
-
-RUN mkdir -p /usr/src/iris
-WORKDIR /usr/src/iris
+FROM python:3.12
 
 COPY requirements.txt .
-RUN pip3 install -r requirements.txt
+RUN pip install -r requirements.txt
 
 COPY . .
 
--- iris/router/iris_classifier_router.py
+++ iris/router/iris_classifier_router.py
@@ -1,14 +1,42 @@
 from fastapi import APIRouter
 from starlette.responses import JSONResponse
-
+from fastapi import UploadFile
 from iris.iris_classifier import IrisClassifier
 from iris.models import Iris
+import json
 
 
 router = APIRouter()
 
 
-@router.post('/classify_iris')
-def extract_name(iris_features: Iris):
-    iris_classifier = IrisClassifier()
-    return JSONResponse(iris_classifier.classify_iris(iris_features))
+@router.post("/upload")
+def upload(file: UploadFile):
+    content = file.file.read()
+    content_json = json.loads(content)
+    print(content_json)
+    return {"filename": file.filename}
+
+
+@router.post("/classify")
+def classify(inputs: UploadFile):
+
+    try:
+        # Parse inputs into JSON
+        iris_features = json.loads(inputs.file.read())
+
+        # Output
+        classified_iris = []
+
+        # Init classifier
+        iris_classifier = IrisClassifier()
+
+        # Classify all input instances
+        for input in iris_features:
+            iris = Iris(**input)
+            result = iris_classifier.classify_iris(iris)
+            classified_iris.append({"features": input, "output": result})
+
+        return JSONResponse(classified_iris)
+
+    except Exception as e:
+        return JSONResponse({"error": str(e)})
