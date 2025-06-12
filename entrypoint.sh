dd6faf815ae91c580876d40d9f8cce924e8edfc9
Sanjana Udar <sanjana.udar@pinq2.com>
2025-06-12 10:31:28 -0400

    condition to handle no input file

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,11 @@ else
   echo "env.sh missing or empty"
 fi
 
+if [ -f "$KIT_INPUTS_FILE" ]; then
+  echo "Found input file, processing…"
+  # do work
+else
+  echo "No input file supplied, continuing without it"
+fi
+
 exec "$@"
